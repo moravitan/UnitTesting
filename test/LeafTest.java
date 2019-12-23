5fd8b4db8cb7e88e95ee6e7eddafd5ce3f76c062
moravit <moravit@post.bgo.ac.il>
2019-12-23 11:06:28 +0200

    leaf test update

--- test/LeafTest.java
+++ test/LeafTest.java
@@ -1,22 +1,35 @@
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 public class LeafTest {
 
-    Leaf leaf;
+     Leaf leaf;
+     FileSystem fileSystem;
 
     @Before
-    public void setUp() throws OutOfSpaceException {
-        FileSystem fs = new FileSystem(10);
-        leaf = new Leaf("leaf",10);
+    public void setUp() {
+        fileSystem = new FileSystem(10);
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void allocateOutOfSpace() throws OutOfSpaceException{
+        leaf = new Leaf("leaf",12);
+        assertEquals("leaf",leaf.name);
+
     }
 
     @Test
-    public void d() throws OutOfSpaceException {
-        FileSystem fs = new FileSystem(10);
-        leaf = new Leaf("leaf",10);
+    public void allocate() throws OutOfSpaceException{
+        leaf = new Leaf("leaf",2);
+        assertEquals("leaf",leaf.name);
+        Leaf leaf1 = new Leaf("leaf1",7);
+        String[] path = {"leaf1"};
+//        assertNotNull(fileSystem.FileExists(path));
+
+
 
     }
-
-
 }
